7d46a506c6927b4910a05741147e6c65bca38eda
Vinicius Lemos <vinicius.lemos@educacional.com.br>
2026-06-19 16:18:19 -0300

    Fix external id condition for target role trust policy

--- terraform/prowler-target-role/main.tf
+++ terraform/prowler-target-role/main.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "trust" {
     }
 
     dynamic "condition" {
-      for_each = var.external_id == "" ? [] : [var.external_id]
+      for_each = var.external_id == "" ? {} : { external_id = var.external_id }
 
       content {
         test     = "StringEquals"
